bd86f1e53776ee8c8aba5725e14f2154b35a1a83
rosV4 <rosV4@kinalisoft.eu>
2018-07-30 13:05:54 +0200

    changed object type

--- art_arcode_detector/src/detector.py
+++ art_arcode_detector/src/detector.py
@@ -50,7 +50,7 @@ class ArCodeDetector:
 
                 object_type = None
                 if aid in [5001, 5002]:
-                    object_type = self.art.get_object_type("Modry_kontejner")
+                    object_type = self.art.get_object_type("blue_bin")
                     
                 elif aid in [50, 51, 52, 53, 54, 55, 56, 57, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010,
                            1011, 1012, 1013, 1014, 1015, 1016, 1017, 1018, 1019, 1020, 1021, 1022, 1023, 1024, 1025]:
